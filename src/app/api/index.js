ed557d418a029c825211e4ca2d261c9de83c69dc
antonkobelev <antonkobelev85@mail.ru>
2023-02-19 13:50:15 +0500

    teacher pagination with eslint

--- src/app/api/index.js
+++ src/app/api/index.js
@@ -1,5 +1,3 @@
-import * as users from "./fake.api/user.api"
-const API = {
-    users
-}
-export default API
+import * as users from "./fake.api/user.api";
+const API = { users };
+export default API;
--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Pagination from "./pagination";
 import User from "./user";
+import { paginate } from "../utils/paginate";
+import PropTypes from "prop-types";
+
 const Users = ({ users, ...rest }) => {
+    const count = users.length;
+
+    const pageSize = 4;
+    const [currentPage, setCurrentPage] = useState(1);
+    const handlePageChange = (pageIndex) => {
+        setCurrentPage(pageIndex);
+    };
+    const userCrop = paginate(users, currentPage, pageSize);
+    useEffect(() => {
+        if (count > 0) {
+            if (userCrop.length === 0) setCurrentPage((prev) => prev - 1);
+        }
+    }, [userCrop]);
     return (
         <>
-            {users.length > 0 && (
+            {count > 0 && (
                 <table className="table">
                     <thead>
                         <tr>
@@ -17,14 +34,24 @@ const Users = ({ users, ...rest }) => {
                         </tr>
                     </thead>
                     <tbody>
-                        {users.map((user) => (
+                        {userCrop.map((user) => (
                             <User key={user._id} {...rest} {...user} />
                         ))}
                     </tbody>
                 </table>
             )}
+            <Pagination
+                itemsCount={count}
+                pageSize={pageSize}
+                currentPage={currentPage}
+                onPageChange={handlePageChange}
+            />
         </>
     );
+};
+
+Users.propTypes = {
+    users: PropTypes.array.isRequired
 };
 
 export default Users;
